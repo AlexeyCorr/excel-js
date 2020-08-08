75ea67a85ed8502358a916751a0ab576c31fb285
AlexeyCorr <alexeycorr@gmail.com>
2020-08-08 16:47:26 +0300

    add redux logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -6,8 +6,8 @@ export class ExcelComponent extends DomListener {
     this.name = options.name || ''
     this.store = options.store
     this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
     this.unsubscribers = []
-    this.storeSub = null
 
     this.prepare()
   }
@@ -31,8 +31,10 @@ export class ExcelComponent extends DomListener {
     this.store.dispatch(action)
   }
 
-  $subscribe(fn) {
-    this.storeSub = this.store.subscribe(fn)
+  storeChanged() {}
+
+  isWatching(key) {
+    return this.subscribe.includes(key)
   }
 
   init() {
@@ -42,6 +44,5 @@ export class ExcelComponent extends DomListener {
   destroy() {
     this.removeDOMListeners()
     this.unsubscribers.forEach(unsub => unsub())
-    this.storeSub.unsubscribe()
   }
 }
